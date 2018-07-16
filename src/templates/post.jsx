df07f673a9adbd8f18be3df96294b5e2cee50019
Thada Wangthammang <mildronize@gmail.com>
2018-07-17 02:25:44 +0700

    Fix container size for tablet

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -45,7 +45,7 @@ export default class PostTemplate extends React.Component {
           <div className="container">
 
             <div className="columns">
-              <div className="column is-8-desktop is-offset-2-desktop">
+              <div className="column is-8-desktop is-9-tablet">
                 <div className="content is-size-5 is-size-6-touch">
 
                   <Helmet>
